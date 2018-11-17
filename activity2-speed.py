861b988889860b72bf4ad0364e2e73892312acfb
retrobrian <32041112+retrobrian@users.noreply.github.com>
2018-11-17 15:08:54 -0800

    updated comments

--- activity2-speed.py
+++ activity2-speed.py
@@ -1,19 +1,20 @@
 # Original gui_slider written by Simon Monk
 # Updates and comments by Brian Cox
-# Updated 11-8-18
+# Updated 11-17-18
 # Expecting Python 3
 
-from tkinter import *
-import RPi.GPIO as GPIO
-import time
+from tkinter import *            #Loads Python's GUI package
+import RPi.GPIO as GPIO          #Loads the module to support for the RPi's GPIO and gives it the name GPIO
+import time                      #Loads support Python time support 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.OUT)
-pwm = GPIO.PWM(24, 500)
-pwm.start(0)
+GPIO.setmode(GPIO.BCM)           #Sets board numbersing using the Broadcom SOC numbering scheme. As opposed to BOARD which refers to the pin numbers on the P1 header of the RPi's board
+GPIO.setup(24, GPIO.OUT)         #Sets GPIO pin 24 as an output
+pwm = GPIO.PWM(24, 500)          #Loads PWM support on pin 24 and sets the frequency at 500Hz
+pwm.start(0)                     #Starts PWM on pin 24 and sets the duty cycle to 0. (range is 0 to 100)
 
+#Setup support for window and slider
 class App:
-	
+    
     def __init__(self, master):
         frame = Frame(master)
         frame.pack()
@@ -23,10 +24,11 @@ class App:
 
 
     def update(self, duty):
-        pwm.ChangeDutyCycle(float(duty))
+        pwm.ChangeDutyCycle(float(duty))          #Changes duty cycle of PWM based on the position of the slider
 
+#Sets parmeters for the main window
 root = Tk()
-root.wm_title('PWM Percentage')
+root.wm_title('Motor Speed')
 app = App(root)
 root.geometry("400x50+0+0")
 root.mainloop()
